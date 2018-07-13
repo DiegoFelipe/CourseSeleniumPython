e83dfb63427cf504f86cbfbf0633502df55c8c0c
Diego Felipe <diegofelipe.14@gmail.com>
2018-07-12 22:28:06 -0400

    adding first quiz

---
+++ quizes/quiz1.py
@@ -0,0 +1,10 @@
+# Complete the method by making sure it returns 42. .
+def return_42():
+    # Complete method here
+    # 'pass' just means "do nothing". Make sure to delete this!
+    return 42
+
+# Create a method below, called my_method, that takes two arguments and returns the result
+#of its two arguments multiplied together.
+def my_method(num1, num2):
+    return num1 * num2
